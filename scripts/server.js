e41334016d86b340471a73d322b757e9af8be4b1
Przemek Urbaniak <przemyslaw.urbaniak@solwit.com>
2016-10-04 17:04:20 +0200

    Reading data from serial

--- scripts/server.js
+++ scripts/server.js
@@ -5,6 +5,43 @@ var app = express();
 var rootPath = path.normalize(__dirname + "/../");
 var bodyParser = require("body-parser");
 
+//-----------------------
+var http = require('http');
+var serialport = require("serialport");
+var SerialPort = serialport.SerialPort;
+var portName = "/dev/cu.usbmodem1411";
+
+var myPort = new SerialPort(portName, {
+    baudRate:9600,
+    parser: serialport.parsers.readline("\r\n")
+});
+
+myPort.on('open', onOpen);
+myPort.on('data', onData);
+
+var data =["aa"];
+
+function onOpen(){
+    console.log("openconncetion");
+}
+
+function onData(data){
+    console.log('on data'+data);
+    //data.push(data);
+}
+function onRequest(request, response){
+    console.log("user made a request"+request.url);
+    response.writeHead(200, {"Content-Type": "text/plain"});
+    response.write("Here is some data");
+    response.end;
+}
+
+
+//http.createServer(onRequest).listen(8888);
+//console.log("server is running");
+
+//-----------------------
+
 
 
 app.use(bodyParser.urlencoded({extended: true}));
